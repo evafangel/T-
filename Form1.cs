c51ece79d5c1ac8e9caec3d52203a6edfc0f939e
evafangel <98516576+evafangel@users.noreply.github.com>
2023-03-06 15:01:20 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointAndRectangle
+{
+    public partial class FormPointAndRectangle : Form
+    {
+        public FormPointAndRectangle()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPointAndRectangle_Load(object sender, EventArgs e)
+        {
+            Draw(); //метод в който е всичко за показването в кутията
+        }
+
+        private void FormPointAndRectangle_Resize(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownX1_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownX2_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownY2_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownX_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownY_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+
+            // Взима кординатите на правоъгълника и точката от Form
+            var x1 = this.numericUpDownX1.Value;
+            var y1 = this.numericUpDownY1.Value;
+            var x2 = this.numericUpDownX2.Value;
+            var y2 = this.numericUpDownY2.Value;
+            var x = this.numericUpDownX.Value;
+            var y = this.numericUpDownY.Value;
+
+            // Показва местоположението на точката дали ще е Вътре/Вън или на рамката на правоъгълника
+            DisplayPointLocation(x1, y1, x2, y2, x, y);
+
+            var minX = Min(x1, x2, x);
+            var maxX = Max(x1, x2, x);
+            var minY = Min(y1, y2, y);
+            var maxY = Max(y1, y2, y);
+            var diagramWidth = maxX - minX;
+            var diagramHeight = maxY - minY;
+            var ratio = 1.0m;
+            var offset = 10;
+            if (diagramWidth != 0 && diagramHeight != 0)
+            {
+                var ratioX = (pictureBox.Width - 2 * offset - 1) / diagramWidth;
+                var ratioY = (pictureBox.Height - 2 * offset - 1) / diagramHeight;
+                ratio = Math.Min(ratioX, ratioY);
+            }
+
+            // Изчислява кординатите на правоъгълника
+            var rectLeft = offset + (int)Math.Round((Math.Min(x1, x2) - minX) * ratio);
+            var rectTop = offset + (int)Math.Round((Math.Min(y1, y2) - minY) * ratio);
+            var rectWidth = (int)Math.Round(Math.Abs(x2 - x1) * ratio);
+            var rectHeight = (int)Math.Round(Math.Abs(y2 - y1) * ratio);
+            var rect = new Rectangle(rectLeft, rectTop, rectWidth, rectHeight);
+
+            // Изчислява кординатите на точката
+            var pointX = (int)Math.Round(offset + (x - minX) * ratio);
+            var pointY = (int)Math.Round(offset + (y - minY) * ratio);
+            var pointRect = new Rectangle(pointX - 2, pointY - 2, 5, 5);
+
+            // Показва/нарисува правоъгълника и точката
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (var g = Graphics.FromImage(pictureBox.Image))
+            {
+                //Диаграма с цвят(Бял)
+                g.Clear(Color.White);
+
+                // Показва правоъгълника (в размерите на кутията)
+                var pen = new Pen(Color.Blue, 3);
+                g.DrawRectangle(pen, rect);
+
+                // Същото но за точката ^
+                pen = new Pen(Color.DarkBlue, 5);
+                g.DrawEllipse(pen, pointRect);
+            }
+        }
+
+        private decimal Min(decimal val1, decimal val2, decimal val3)
+        {
+            return Math.Min(val1, Math.Min(val2, val3));
+        }
+
+        private decimal Max(decimal val1, decimal val2, decimal val3)
+        {
+            return Math.Max(val1, Math.Max(val2, val3));
+        }
+
+
+
+        private void DisplayPointLocation(decimal x1, decimal y1, decimal x2, decimal y2, decimal x, decimal y)
+        {
+            var left = Math.Min(x1, x2);
+            var right = Math.Max(x1, x2);
+            var top = Math.Min(y1, y2);
+            var bottom = Math.Max(y1, y2);
+
+            if (x > left && x < right && y > top && y < bottom)
+            {
+                this.labelLocation.Text = "Вътре";
+                this.labelLocation.BackColor = Color.Blue;
+            }
+            else if (x < left || x > right || y < top || y > bottom)
+            {
+                this.labelLocation.Text = "Отвън";
+                this.labelLocation.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                this.labelLocation.Text = "Рамка";
+                this.labelLocation.BackColor = Color.Gold;
+            }
+        }
+    }
+}
+
